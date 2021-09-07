94b91ec1ec354cd6595953c69f32c925ec180020
onezmy <cyonezmy@gmail.com>
2021-09-06 22:31:58 -0400

    Add files via upload

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,9 +6,8 @@ public class Planet {
 	public double yyVel; // Its current velocity in the y direction
 	public double mass; // Its mass
 	public String imgFileName; // The name of the file that corresponds to the image that depicts the planet
-	public static double G = 6.67e-11;
+	private static double G = 6.67e-11;
 
-	public Planet(){};
 
 	public Planet(double xP, double yP, double xV, double yV, double m, String img){
 		xxPos = xP;
